0f0f632af5aeb6290b7a86b521a818157ec98362
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-01-04 18:49:25 +0000

    Add goal behaviour

---
+++ Assets/scripts/Goal.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[AddComponentMenu ("Vistage/Goal")]
+public class Goal : MonoBehaviour {
+
+	static public bool isGoalMet;
+
+	private void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag ("projectile"))
+		{
+			isGoalMet = true;
+
+			Renderer ren = GetComponent<Renderer> ();
+			Color c = ren.material.color;
+			c.a = 1f;
+			ren.material.color = c;
+		}
+	}
+}
